189f394da91dd6afbc08f2814bee0ec3a64bcc3c
SaiDuc <saipandian97@gmail.com>
2020-02-12 11:59:19 +0000

    Initial Commit

---
+++ lattice.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Lattice:
+    """
+    Class of lattice.
+    Takes init parameter: number of dimensions
+    """
+    def __init__(self, dimensions):
+        self.dimensions = dimensions
+        self.coordinates = np.zeros(dimensions, dtype='int')
+
+    def move(self):
+        """
+        takes input directions array and adds to coordinates
+        """
+        direction = np.random.randint(0, self.dimensions)
+        distance = np.random.choice([-1,1])
+        
+        self.coordinates[direction] += distance
+
+    def caught(self, probability):
+        
+        
+        
