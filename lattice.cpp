92f08f328f3b0f3074f157cfb24b75a1896a1903
SaiDuc <saipandian97@gmail.com>
2020-02-13 01:57:59 +0000

    Fixed indentation
    Made the style of indentation a little prettier

--- lattice.cpp
+++ lattice.cpp
@@ -9,72 +9,71 @@ using namespace std;
 
 class Lattice {
 public:
+    // variables
+    int dimensions;
+    vector<int> coordinates;
+    default_random_engine generator;
 
-  // variables
-  int dimensions;
-  vector<int> coordinates;
-  default_random_engine generator;
+    // constructor
+    Lattice(int d){
+	dimensions = d;
 
-  // constructor
-  Lattice(int d){
-    dimensions = d;
-
-    // fills coordinate array with zeros
-    // can probably be done with coordinates.assign(...)
-    for(int i=0; i<d; i++){
-      coordinates.push_back(0);
+	// fills coordinate array with zeros
+	// can probably be done with coordinates.assign(...)
+	for(int i=0; i<d; i++){
+	    coordinates.push_back(0);
+	}
     }
-  }
 
   // move function
-  void move(){
-    int direction = rand()%dimensions;
-    int distance = rand()%2;
+    void move(){
+	int direction = rand()%dimensions;
+	int distance = rand()%2;
 
-    // so that distance is always -1 or 1
-    if(distance == 0){
-      distance = -1;
+	// so that distance is always -1 or 1
+	if(distance == 0){
+	    distance = -1;
+	}
+	coordinates[direction] = coordinates[direction] + distance;
     }
-
-    coordinates[direction] = coordinates[direction] + distance;
-  }
 
   // caught function
   bool caught(double probability){
-    uniform_real_distribution<double> distribution(0.0,1.0);
-    double number = distribution(generator);
-    if(number < probability){
-      // cout << "true" << endl;
-      return true;
-    }
-    // cout << "false" << endl;
-    return false;
+      uniform_real_distribution<double> distribution(0.0,1.0);
+      double number = distribution(generator);
+
+      if(number < probability){
+	  // cout << "true" << endl;
+	  return true;
+      }
+      // cout << "false" << endl;
+      return false;
   }
 
 };
 
 int main(){
-  Lattice lattice(3);
-  vector<int> data;
+    Lattice lattice(3);
+    vector<int> data;
 
-  // run the random walk
-  for(int i=0; i<1000; i++){
-    bool caught = false;
-    int time = 0;
-    while(caught != true){
-      time++;
-      lattice.move();
-      caught = lattice.caught(0.1);
+    // run the random walk
+    for(int i=0; i<1000000; i++){
+	bool caught = false;
+	int time = 0;
+	while(caught != true){
+	    time++;
+	    lattice.move();
+	    caught = lattice.caught(0.1);
+	}
+	// append to data
+	data.push_back(time);
     }
-    // append to data
-    data.push_back(time);
-  }
 
-  ofstream myfile;
-  myfile.open("data.dat");
-  for(int i=0; i<data.size(); i++){
-    myfile << data[i] << "\n";
-  }
-  myfile.close();
-  return 0;
+    ofstream myfile;
+    myfile.open("data.dat");
+    for(int i=0; i<data.size(); i++){
+	myfile << data[i] << "\n";
+    }
+    myfile.close();
+    return 0;
 }
