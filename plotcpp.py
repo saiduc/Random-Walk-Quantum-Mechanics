e259116e58d522d273726b07865a016112f4adf6
SaiDuc <saipandian97@gmail.com>
2020-02-13 19:35:27 +0000

    Made straight line plot function and changed exponential function
    Changed exponential function to plot data as is, with only y axis scale being
    log. Also added run_model function that compiles and executes c++ code

--- plotcpp.py
+++ plotcpp.py
@@ -1,14 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from subprocess import call
+
+
+def run_model():
+    print('compiling')
+    call(['g++', 'lattice.cpp', '-o', 'lattice'])
+    print('compilation successful')
+
+    print('running model')
+    call(['./lattice'])
+    print('model complete')
 
 
 def exp_curve(n, q, arrest):
     return q * np.exp(-1 * arrest * n)
-
-
-def straight_line(n, arrest, c):
-    return arrest * n + c
 
 
 def exp_plot(data):
@@ -35,22 +42,61 @@ def line_plot(data):
     plt.style.use("seaborn")
 
     hist = plt.hist(data, nbins, normed=True)
+    plt.clf()
 
     x = hist[1][:-1]
-    y = np.log(hist[0])
+    y = hist[0]
+    popt, _ = curve_fit(exp_curve, x, y)
 
-    popt, _ = curve_fit(straight_line, x, y)
-
-    plt.plot(x, straight_line(x, *popt), c='r', label="Fitted Line")
+    plt.yscale("log")
+    plt.plot(x, y, marker='.', ls=' ')
+    plt.plot(x, exp_curve(x, *popt), c='r', label="Fitted Curve")
 
     plt.ylabel('Probability')
-    plt.xlabel('Time Survived (log no. of steps)')
+    plt.xlabel('Time Survived (no. of steps)')
     plt.legend()
     plt.show()
     return popt[1]
 
 
+# def straight_line(n, arrest, c):
+#     return arrest * n + c
+
+# def line_plot(data):
+#     """
+#     This is broken since I should not be converting the data to log.
+#     I should just be plotting the yscale in log
+#     This is because if I convert the data to logs and do curve_fit,
+#     the straight_line function has the wrong weightings
+#     """
+#     nbins = int(max(data) - 1)
+#     plt.style.use("seaborn")
+
+#     hist = plt.hist(data, nbins, normed=True)
+#     plt.clf()
+
+#     x = hist[1][:-1]
+#     y = np.log(hist[0])
+
+#     # Removing -inf values
+#     indices = [i for i in range(len(y)) if np.isinf(y[i])]
+#     x = np.delete(x, indices)
+#     y = np.delete(y, indices)
+
+#     popt, _ = curve_fit(straight_line, x, y)
+
+#     plt.plot(x, straight_line(x, *popt), c='r', label="Fitted Line")
+
+#     plt.plot(x,y)
+
+#     plt.ylabel('Probability (log K)')
+#     plt.xlabel('Time Survived (no. of steps)')
+#     plt.legend()
+#     plt.show()
+#     return popt[1]
+
+
 if __name__ == "__main__":
     data = np.loadtxt("data.dat")
     prob_arrest_curve = exp_plot(data)
-    # prob_arrest_line = line_plot(data)
+    prob_arrest_line = line_plot(data)
