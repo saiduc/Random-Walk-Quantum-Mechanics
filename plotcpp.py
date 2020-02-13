5b4d02bb1d3cc51f9e1a7b0358cfeb3c974d2052
SaiDuc <saipandian97@gmail.com>
2020-02-13 13:57:51 +0000

    Moved code into functions for easy extensions

--- plotcpp.py
+++ plotcpp.py
@@ -3,23 +3,31 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def func(n, q, arrest):
+def exp_curve(n, q, arrest):
     return q * np.exp(-1 * arrest * n)
 
 
-data = np.loadtxt("data.dat")
-nbins = int(max(data) - 1)
-plt.style.use("seaborn")
+def exp_plot(data=np.loadtxt("data.dat")):
+    nbins = int(max(data) - 1)
+    plt.style.use("seaborn")
 
-hist = plt.hist(data, nbins, normed=True)
+    hist = plt.hist(data, nbins, normed=True)
 
-x = hist[1][:-1]
-y = hist[0]
-popt, _ = curve_fit(func, x, y)
+    x = hist[1][:-1]
+    y = hist[0]
+    popt, _ = curve_fit(exp_curve, x, y)
 
-plt.plot(x, func(x, *popt), c='r', label="Fitted Curve")
+    plt.plot(x, exp_curve(x, *popt), c='r', label="Fitted Curve")
 
-plt.ylabel('Probability')
-plt.xlabel('Time Survived (no. of steps)')
-plt.legend()
-plt.show()
+    plt.ylabel('Probability')
+    plt.xlabel('Time Survived (no. of steps)')
+    plt.legend()
+    plt.show()
+
+
+def line_plot(data=np.loadtxt("data.dat")):
+    pass
+
+
+if __name__ == "__main__":
+    exp_plot()
