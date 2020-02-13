1db3de6470cbc38b73691426a95656bfe01298d2
SaiDuc <saipandian97@gmail.com>
2020-02-13 21:36:10 +0000

    Fixed indentation

--- lattice.cpp
+++ lattice.cpp
@@ -10,10 +10,10 @@ using namespace std;
 
 
 /*
-I use this method instead of rand because rand is seeded to time
-And each use of rand seeds to time (not previous value like in python)
-But the time is in seconds, so over many iterations, many random
-values are repeated
+  I use this method instead of rand because rand is seeded to time
+  And each use of rand seeds to time (not previous value like in python)
+  But the time is in seconds, so over many iterations, many random
+  values are repeated
 */
 
 // set up random generator and define seed for each
@@ -22,85 +22,84 @@ mt19937 generator(rd());
 
 class Lattice {
 public:
-    // variables
-    int dimensions;
-    vector<int> coordinates;
+	// variables
+	int dimensions;
+	vector<int> coordinates;
 
-    // constructor
-    Lattice(int d){
-	dimensions = d;
+	// constructor
+	Lattice(int d){
+		dimensions = d;
 
-	// fills coordinate array with zeros
-	// can probably be done with coordinates.assign(...)
-	for(int i=0; i<d; i++){
-	    coordinates.push_back(0);
+		// fills coordinate array with zeros
+		// can probably be done with coordinates.assign(...)
+		for(int i=0; i<d; i++){
+			coordinates.push_back(0);
+		}
 	}
-    }
 
-  // move function
-    void move(){
-	// int direction = rand()%dimensions;
-	// int distance = rand()%2;
+	// move function
+	void move(){
+		// int direction = rand()%dimensions;
+		// int distance = rand()%2;
 
-	uniform_int_distribution<> distribution1(0,dimensions-1);
-	int direction = distribution1(generator);
+		uniform_int_distribution<> distribution1(0,dimensions-1);
+		int direction = distribution1(generator);
 
-	uniform_int_distribution<> distribution2(0,1);
-	int distance = distribution2(generator);
+		uniform_int_distribution<> distribution2(0,1);
+		int distance = distribution2(generator);
 
-	// so that distance is always -1 or 1
-	if(distance == 0){
-	    distance = -1;
+		// so that distance is always -1 or 1
+		if(distance == 0){
+			distance = -1;
+		}
+		coordinates[direction] = coordinates[direction] + distance;
 	}
-	coordinates[direction] = coordinates[direction] + distance;
-    }
 
-  // caught function
-    bool caught(double probability){
-      uniform_real_distribution<double> distribution3(0.0,1.0);
-      double number = distribution3(generator);
+	// caught function
+	bool caught(double probability){
+		uniform_real_distribution<double> distribution3(0.0,1.0);
+		double number = distribution3(generator);
 
-      if(number < probability){
-	  return true;
-      }
-      return false;
-  }
+		if(number < probability){
+			return true;
+		}
+		return false;
+	}
 
 };
 
 int main(){
 
-    srand(time(NULL));   
+	srand(time(NULL));   
 
-    int dimen = 4;
-    Lattice lattice(dimen);
+	int dimen = 4;
+	Lattice lattice(dimen);
     
-    vector<int> data;
+	vector<int> data;
 
-    // run the random walk
-    for(int i=0; i<1000; i++){
-	bool caught = false;
-	int time = 0;
-	while(caught != true){
-	    time++;
-	    lattice.move();
-
-	    caught = lattice.caught(0.1);
+	// run the random walk
+	for(int i=0; i<1000; i++){
+		bool caught = false;
+		int time = 0;
+		while(caught != true){
+			time++;
+			lattice.move();
+			caught = lattice.caught(0.1);
+		}
+		// append to data
+		data.push_back(time);
 	}
-	// append to data
-	data.push_back(time);
-    }
 
-    // for debugging 
-    // for(int i=0; i<data.size(); i++){
-    // 	cout << data[i] << "\n";
-    // }
+	// for debugging 
+	// for(int i=0; i<data.size(); i++){
+	// 	cout << data[i] << "\n";
+	// }
 
-    ofstream myfile;
-    myfile.open("data.dat");
-    for(int i=0; i<data.size(); i++){
-    	myfile << data[i] << "\n";
-    }
-    myfile.close();
-    return 0;
+	ofstream myfile;
+	myfile.open("data.dat");
+	for(int i=0; i<data.size(); i++){
+		myfile << data[i] << "\n";
+	}
+	myfile.close();
+	return 0;
 }
