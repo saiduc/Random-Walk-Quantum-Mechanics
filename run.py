77f732c558350dc08eb21923f85c57e076330191
SaiDuc <saipandian97@gmail.com>
2020-02-13 19:34:32 +0000

    Moved plotting into separate functions and made straight line plot

--- run.py
+++ run.py
@@ -20,14 +20,11 @@ def iterate(dimensions, catch_prob, number_iterations):
     return data
 
 
-def func(n, q, arrest):
+def exp_curve(n, q, arrest):
     return q * np.exp(-1 * arrest * n)
 
 
-if __name__ == "__main__":
-
-    data = iterate(3, 0.1, 1000)
-
+def exp_plot(data):
     nbins = max(data) - 1
     plt.style.use("seaborn")
 
@@ -35,11 +32,40 @@ if __name__ == "__main__":
 
     x = hist[1][:-1]
     y = hist[0]
-    popt, _ = curve_fit(func, x, y)
+    popt, _ = curve_fit(exp_curve, x, y)
 
-    plt.plot(x, func(x, *popt), c='r', label="Fitted Curve")
+    plt.plot(x, exp_curve(x, *popt), c='r', label="Fitted Curve")
 
     plt.ylabel('Probability')
     plt.xlabel('Time Survived (no. of steps)')
     plt.legend()
     plt.show()
+    return popt[1]
+
+
+def line_plot(data):
+    nbins = max(data) - 1
+    plt.style.use("seaborn")
+
+    hist = plt.hist(data, nbins, normed=True)
+    plt.clf()
+
+    x = hist[1][:-1]
+    y = hist[0]
+    popt, _ = curve_fit(exp_curve, x, y)
+
+    plt.yscale("log")
+    plt.plot(x, y, marker='.', ls=' ')
+    plt.plot(x, exp_curve(x, *popt), c='r', label="Fitted Curve")
+
+    plt.ylabel('Probability')
+    plt.xlabel('Time Survived (no. of steps)')
+    plt.legend()
+    plt.show()
+    return popt[1]
+
+
+if __name__ == "__main__":
+    data = iterate(3, 0.1, 1000)
+    prob_arrest_curve = exp_plot(data)
+    prob_arrest_line = line_plot(data)
