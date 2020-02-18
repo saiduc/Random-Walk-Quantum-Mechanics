f5a30e4b6ed83a739e0a34143cb2da33f9cd85f0
SaiDuc <saipandian97@gmail.com>
2020-02-18 23:07:34 +0000

    Modified run_model function to allow for infinite square well
    Made some other changes to plotting function, so that a certain number of values
    at the start of the plot can be skipped in the plot.
    
    Deleted some unnecessary comments

--- plotcpp.py
+++ plotcpp.py
@@ -4,13 +4,19 @@ from scipy.optimize import curve_fit
 from subprocess import call
 
 
-def run_model(dimen, prob, iteration):
+def run_model(dimen, iteration, prob=0.1, potential="", boundary=3):
     print('Compiling...')
     call(['g++', 'lattice.cpp', '-o', 'lattice'])
     print('Compilation successful!')
 
     print('Running model...')
-    call(['./lattice', str(dimen), str(prob), str(iteration)])
+    call([
+        './lattice',
+        str(dimen),
+        str(iteration),
+        str(prob), potential,
+        str(boundary)
+    ])
     print('Model complete!')
 
 
@@ -18,14 +24,15 @@ def exp_curve(n, q, arrest):
     return q * np.exp(-1 * arrest * n)
 
 
-def exp_plot(data, show=True):
+def exp_plot(data, show=True, skip=False):
     nbins = int(max(data) - 1)
     plt.style.use("seaborn")
 
     hist = plt.hist(data, nbins, normed=True, align='mid')
 
-    x = hist[1][:-1]
-    y = hist[0]
+    x = hist[1][skip:-1]
+    y = hist[0][skip:]
+
     popt, _ = curve_fit(exp_curve, x, y)
 
     if show:
@@ -38,15 +45,16 @@ def exp_plot(data, show=True):
     return popt[1]
 
 
-def line_plot(data, show=True):
+def line_plot(data, show=True, skip=False):
     nbins = int(max(data) - 1)
     plt.style.use("seaborn")
 
     hist = plt.hist(data, nbins, normed=True)
     plt.clf()
 
-    x = hist[1][:-1]
-    y = hist[0]
+    x = hist[1][skip:-1]
+    y = hist[0][skip:]
+
     popt, _ = curve_fit(exp_curve, x, y)
 
     if show:
@@ -61,50 +69,21 @@ def line_plot(data, show=True):
     return popt[1]
 
 
-# def straight_line(n, arrest, c):
-#     return arrest * n + c
-
-# def line_plot(data):
-#     """
-#     This is broken since I should not be converting the data to log.
-#     I should just be plotting the yscale in log
-#     This is because if I convert the data to logs and do curve_fit,
-#     the straight_line function has the wrong weightings
-#     """
-#     nbins = int(max(data) - 1)
-#     plt.style.use("seaborn")
-
-#     hist = plt.hist(data, nbins, normed=True)
-#     plt.clf()
-
-#     x = hist[1][:-1]
-#     y = np.log(hist[0])
-
-#     # Removing -inf values
-#     indices = [i for i in range(len(y)) if np.isinf(y[i])]
-#     x = np.delete(x, indices)
-#     y = np.delete(y, indices)
-
-#     popt, _ = curve_fit(straight_line, x, y)
-
-#     plt.plot(x, straight_line(x, *popt), c='r', label="Fitted Line")
-
-#     plt.plot(x,y)
-
-#     plt.ylabel('Probability (log K)')
-#     plt.xlabel('Time Survived (no. of steps)')
-#     plt.legend()
-#     plt.show()
-#     return popt[1]
-
-
 if __name__ == "__main__":
     dimen = 3
+    iteration = 1000000
     prob = 0.1
-    iteration = 100000
-    
-    run_model(dimen, prob, iteration)
+    potential = "square"
+    boundary = 3
+
+    # Any point that has 1 item in the bin has y=1/nbins and nbins = iteration
+    # So if there are 1000 iterations, the lower limit will be 1/1000
+
+    run_model(dimen, iteration, prob=prob)
+    run_model(dimen, iteration, potential=potential, boundary=boundary)
+
     data = np.loadtxt("data.dat")
-    prob_arrest_curve = exp_plot(data, show=True)
-    prob_arrest_line = line_plot(data, show=True)
+    prob_arrest_curve = exp_plot(data, show=True, skip=7)
+    prob_arrest_line = line_plot(data, show=True, skip=7)
+
     print(prob_arrest_curve)
