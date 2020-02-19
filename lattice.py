3463073cdf0c84be4fb577fcba01516678c369e9
SaiDuc <saipandian97@gmail.com>
2020-02-19 16:22:58 +0000

    Made optimisation by combining two if statements into one

--- lattice.py
+++ lattice.py
@@ -23,21 +23,7 @@ class Lattice:
 
     def caught(self, probability=0.1, potential=None, boundary=None, maxSteps=None):
 
-        if maxSteps is None:
-            if potential is None:
-                dist = random.uniform(0, 1)
-                if dist <= probability:
-                    return True
-                else:
-                    return False
-
-            elif potential == "square":
-                for coordinate in self.coordinates:
-                    if not (-1*boundary < coordinate < boundary):
-                        return True
-                return False
-
-        elif self.numberSteps < maxSteps:
+        if (maxSteps is None) or (self.numberSteps < maxSteps):
             if potential is None:
                 dist = random.uniform(0, 1)
                 if dist <= probability:
