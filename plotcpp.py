dff77441dc08ae13076db7487b9ab31f1f91a321
SaiDuc <saipandian97@gmail.com>
2020-03-07 17:18:19 +0000

    Removed unnecessary comments and fixed zig-zag issue
    Now bins are not 1 per time step but 1 per pair of time-steps. This gets around
    the fact that for odd boundary, only odd time-steps are populated and vice-versa
    for even boundary. So you don't get weird zig-zag in the cumulative plots

--- plotcpp.py
+++ plotcpp.py
@@ -50,7 +50,8 @@ def exp_plot(data, show=True, skip=0, start=1):
 
 
 def cum_exp_plot(data, show=True, skip=0, start=1):
-    nbins = int(max(data) - start)
+    nbins = int(int(max(data) - start)/2)
+    print(nbins)
     plt.style.use("seaborn")
 
     hist = plt.hist(data, nbins, normed=True, align='mid')
@@ -103,31 +104,15 @@ def line_plot(data, show=True, skip=0, start=1):
 
 
 def cum_line_plot(data, show=True, skip=0, start=1):
-    nbins = int(max(data) - start)
+    nbins = int(int(max(data) - start)/2)
+    print(nbins)
     plt.style.use("seaborn")
 
     hist = plt.hist(data, nbins, normed=True)
     plt.clf()
 
-    # if detSkip is True:
-    #     val = []
-    #     x = np.arange(20)
-    #     for num in x:
-    #         fit = cum_line_plot(data, show=False, skip=num, start=boundary)
-    #         val.append(fit)
-    #     plt.yscale("log")
-    #     plt.plot(x, val)
-    #     plt.show()
-    #     print(val)
-
     x = hist[1][skip:-1]
     y = hist[0][skip:]
-
-    # val = []
-    # for num in x[:-3]:
-    #     val.append(y[int(num)+1]-y[int(num)])
-    # plt.plot(x[1:-2], val)
-    # plt.show()
 
     cum_y = np.cumsum(y[::-1])[::-1]
 
@@ -142,9 +127,29 @@ def cum_line_plot(data, show=True, skip=0, start=1):
         plt.xlabel('Time Survived (no. of steps)')
         plt.legend()
         plt.show()
-    print(popt[0])
-    print(popt[1])
     return popt[1]
+
+
+def find_remove(data, maxNumber, show=True):
+    N = maxNumber
+    if True:
+        val = []
+        x = range(N)
+        for num in x:
+            fit = cum_line_plot(data, show=False, skip=num, start=boundary)
+            val.append(fit)
+            plt.plot(x, val)
+            plt.show()
+    x = np.arange(0, N, 1)
+    dx = x[1] - x[0]
+    y = val
+    dydx = np.gradient(y, dx)
+
+    if show:
+        plt.plot(x[:-1], dydx[:-1], ls="", marker='o')
+        popt, _ = curve_fit(exp_curve, x, dydx)
+        plt.plot(x, exp_curve(x, *popt))
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -181,30 +186,10 @@ if __name__ == "__main__":
                   potential=potential,
                   boundary=boundary)
         data = np.loadtxt("data.dat")
-        prob_arrest_curve = exp_plot(data, show=False, skip=0, start=boundary)
-        prob_arrest_line = line_plot(data, show=False, skip=0, start=boundary)
+        prob_arrest_curve = exp_plot(data, show=True, skip=0, start=boundary)
+        prob_arrest_line = line_plot(data, show=True, skip=0, start=boundary)
         prob_arrest_cum_curve = cum_exp_plot(data, show=True, skip=0, start=boundary)
-        prob_arrest_cum_line = cum_line_plot(data, show=True, skip=14, start=boundary)
+        prob_arrest_cum_line = cum_line_plot(data, show=True, skip=8, start=boundary)
 
         print(prob_arrest_cum_line * boundary**2)
         print(np.pi**2/8)
-
-        N = 35
-        if True:
-            val = []
-            x = range(N)
-            for num in x:
-                fit = cum_line_plot(data, show=False, skip=num, start=boundary)
-                val.append(fit)
-            plt.plot(x, val)
-            plt.show()
-
-        x = np.arange(0,N,1)
-        dx = x[1] - x[0]
-        y = val
-        dydx = np.gradient(y, dx)
-
-        plt.plot(x[:-1],dydx[:-1], ls="", marker='o')
-        popt, _ = curve_fit(exp_curve, x, dydx)
-        plt.plot(x, exp_curve(x, *popt))
-        plt.show()
