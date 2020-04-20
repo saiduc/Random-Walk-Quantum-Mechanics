ad1ac49286dcfad955a068b0f67281f629f44bba
SaiDuc <saipandian97@gmail.com>
2020-04-20 01:59:01 +0100

    Added extra functions and made plot fonts larger

--- plotcpp.py
+++ plotcpp.py
@@ -1,9 +1,16 @@
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from subprocess import call
 import warnings
 warnings.filterwarnings("ignore")
+
+font = {'family': 'normal',
+        'weight': 'bold',
+        'size': 18}
+
+matplotlib.rc('font', **font)
 
 
 def run_model(dimen, iteration, maxSteps=0, prob=0.1, potential="", boundary=3):
@@ -47,8 +54,9 @@ def exp_plot(data, show=True, skip=0, start=1):
 
         plt.ylabel('Probability of Death')
         plt.xlabel('Time Survived (no. of steps)')
-        plt.legend()
-        # plt.savefig("./Paper/images/exp_plot.pdf")
+        plt.tight_layout()
+        plt.savefig("./Paper/images/exp_plot.pdf")
+        # plt.savefig("./Paper/images/exp_plot_cutoff.pdf")
         plt.show()
     return popt[1]
 
@@ -81,7 +89,9 @@ def cum_exp_plot(data, show=True, skip=0, start=1):
         plt.ylabel('Probability of Survival')
         plt.xlabel('Time Survived (no. of steps)')
         plt.legend()
+        plt.tight_layout()
         plt.savefig("./Paper/images/cum_exp_plot.pdf")
+        # plt.savefig("./Paper/images/cum_exp_plot_cutoff.pdf")
         plt.show()
 
     residuals = cum_y - exp_curve(x, *popt)
@@ -114,10 +124,10 @@ def line_plot(data, show=True, skip=0, start=1):
         plt.plot(x, y, marker='.', ls=' ')
         # plt.plot(x, exp_curve(x, *popt), c='r', label="Fitted Curve")
 
-        plt.ylabel('log(Probability of Death)')
+        plt.ylabel('Probability of Death')
         plt.xlabel('Time Survived (no. of steps)')
-        plt.legend()
-        # plt.savefig("./Paper/images/line_plot.pdf")
+        plt.tight_layout()
+        plt.savefig("./Paper/images/line_plot.pdf")
         plt.show()
     return popt[1]
 
@@ -135,6 +145,7 @@ def cum_line_plot(data, show=True, skip=0, start=1):
     y = hist[0][skip:]
 
     cum_y = np.cumsum(y[::-1])[::-1]
+    cum_y = cum_y/max(cum_y)
 
     popt, pcov = curve_fit(exp_curve, x, cum_y, p0=[1.28, 0.019])
 
@@ -143,10 +154,12 @@ def cum_line_plot(data, show=True, skip=0, start=1):
         plt.plot(x, cum_y, marker='.', ls=' ')
         plt.plot(x, exp_curve(x, *popt), c='r', label="Fitted Curve")
 
-        plt.ylabel('log(Probability of Survival)')
+        plt.ylabel('Probability of Survival')
         plt.xlabel('Time Survived (no. of steps)')
         plt.legend()
-        # plt.savefig("./Paper/images/cum_line_plot.pdf")
+        plt.tight_layout()
+        plt.savefig("./Paper/images/cum_line_plot.pdf")
+        # plt.savefig("./Paper/images/cum_line_plot_cutoff.pdf")
         plt.show()
 
     residuals = cum_y - exp_curve(x, *popt)
@@ -159,33 +172,99 @@ def cum_line_plot(data, show=True, skip=0, start=1):
     return popt[1]
 
 
-if __name__ == "__main__":
+def average_value(dimen, iteration, maxSteps=0, prob=0.1, potential="", boundary=3, skip=0, repeats=4):
+    values = []
+    for i in range(repeats):
+        run_model(dimen,
+                  iteration,
+                  maxSteps=maxSteps,
+                  potential=potential,
+                  boundary=boundary)
+        data = np.loadtxt("data.dat")
+        prob_arrest_cum_line = cum_line_plot(data, show=False, skip=skip, start=boundary)
+        values.append(prob_arrest_cum_line * boundary**2)
 
+    items = np.array(values)
+    items = items * boundary**2
+    avg = np.average(items)
+    error = np.std(items)
+    print(avg, " ± ", error)
+    return avg, error
+
+
+def compare_boundaries(start, repeats):
     dimen = 1
     iteration = 1000000
     maxSteps = 0
     potential = "square"
+    boundary = start
+    prob = 0.1
+
+    alldataX = []
+    alldataY = []
+    labels = []
+    allboundaries = []
+
+    number = repeats
+    for i in range(number):
+
+        boundary += i
+
+        run_model(dimen,
+                  iteration,
+                  maxSteps=maxSteps,
+                  potential=potential,
+                  boundary=boundary)
+
+        data = np.loadtxt("data.dat")
+        start = boundary
+        skip = 100
+
+        nbins = int(int(max(data) - start)/2)
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='serif')
+
+        hist = plt.hist(data, nbins, normed=True)
+        plt.clf()
+
+        x = hist[1][skip:-1]
+        y = hist[0][skip:]
+        label = "J: " + str(boundary)
+
+        alldataX.append(x)
+        alldataY.append(y)
+        labels.append(label)
+        allboundaries.append(boundary)
+
+    for i in range(number):
+        cum_y = np.cumsum(alldataY[i][::-1])[::-1]
+
+        popt, _ = curve_fit(exp_curve, alldataX[i], cum_y, p0=[1.28, 0.019])
+
+        label = labels[i] + r", $\lambda J^2$: " + str(popt[1] * allboundaries[i]**2)[:6]
+
+        plt.yscale("log")
+        plt.plot(alldataX[i], cum_y, marker='.', ls=' ', label=label)
+        plt.plot(alldataX[i], exp_curve(alldataX[i], *popt), c='k')
+
+    plt.ylabel('Probability of Survival')
+    plt.xlabel('Time Survived (no. of steps)')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("./Paper/images/multiplot.pdf")
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    dimen = 1
+    iteration = 1000000
+    maxSteps = 2000
+    potential = "square"
     boundary = 20
     prob = 0.1
 
-    # some good parameters
-    # dimen = 1
-    # iteration = 100000
-    # maxSteps = 300
-    # potential = "square"
-    # boundary = 8
-
-    # dimen = 1
-    # iteration = 1000000
-    # maxSteps = 2000
-    # potential = "square"
-    # boundary = 20
-    # prob = 0.1
-
-    # Any point that has 1 item in the bin has y=1/nbins and nbins = iteration
-    # So if there are 1000 iterations, the lower limit will be 1/1000
-
-    # constant potential example
+    # constant potential
     if potential == "":
         run_model(dimen, iteration, prob=prob)
         data = np.loadtxt("data.dat")
@@ -205,90 +284,7 @@ if __name__ == "__main__":
         prob_arrest_cum_curve = cum_exp_plot(data, show=True, skip=100, start=boundary)
         prob_arrest_cum_line = cum_line_plot(data, show=True, skip=100, start=boundary)
 
-        print(prob_arrest_cum_line * boundary**2)
-        print(np.pi**2/8)
+        avg, error = average_value(1, 1000000, maxSteps=0, prob=0.1, potential="square", boundary=20, skip=100, repeats=10)
+        print(avg, " ± ", error[1])
 
-        if False:
-            values = []
-            for i in range(10):
-                run_model(dimen,
-                          iteration,
-                          maxSteps=maxSteps,
-                          potential=potential,
-                          boundary=boundary)
-                data = np.loadtxt("data.dat")
-                prob_arrest_cum_line = cum_line_plot(data, show=False, skip=100, start=boundary)
-                print(prob_arrest_cum_line * boundary**2)
-                values.append(prob_arrest_cum_line * boundary**2)
-
-            gradient = np.average(values)
-            error = np.std(values)
-            print(gradient, " ± ", error)
-
-        if True:
-            dimen = 1
-            iteration = 1000000
-            maxSteps = 0
-            potential = "square"
-            boundary = 8
-            prob = 0.1
-
-            alldataX = []
-            alldataY = []
-            labels = []
-            allboundaries = []
-
-            number = 4
-            for i in range(number):
-
-                boundary += i
-
-                run_model(dimen,
-                          iteration,
-                          maxSteps=maxSteps,
-                          potential=potential,
-                          boundary=boundary)
-
-                data = np.loadtxt("data.dat")
-                start = boundary
-                skip = 100
-                
-                nbins = int(int(max(data) - start)/2)
-                # plt.style.use("seaborn")
-                plt.rc('text', usetex=True)
-                plt.rc('font', family='serif')
-
-                hist = plt.hist(data, nbins, normed=True)
-                plt.clf()
-
-                x = hist[1][skip:-1]
-                y = hist[0][skip:]
-                label = "J: " + str(boundary)
-
-                alldataX.append(x)
-                alldataY.append(y)
-                labels.append(label)
-                allboundaries.append(boundary)
-
-                
-
-            for i in range(number):
-                cum_y = np.cumsum(alldataY[i][::-1])[::-1]
-
-                popt, _ = curve_fit(exp_curve, alldataX[i], cum_y, p0=[1.28, 0.019])
-
-                label = labels[i] + r", $\lambda J^2$: " + str(popt[1] * allboundaries[i]**2)
-
-                plt.yscale("log")
-                plt.plot(alldataX[i], cum_y, marker='.', ls=' ', label=label)
-                plt.plot(alldataX[i], exp_curve(alldataX[i], *popt), c='k')
-
-            plt.ylabel('log(Probability of Survival)')
-            plt.xlabel('Time Survived (no. of steps)')
-            plt.legend()
-            plt.savefig("./Paper/images/multiplot.pdf")
-            
-
-            
-            
-
+        compare_boundaries(14, 4)
