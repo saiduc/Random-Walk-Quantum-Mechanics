7a15cca0a0e7e0b4f5fb81e76334f0890a48e61f
SaiDuc <saipandian97@gmail.com>
2020-04-20 02:02:19 +0100

    Made plot fonts larger, so they are suitable for paper

--- cartoon.py
+++ cartoon.py
@@ -1,6 +1,13 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+
+font = {'family': 'normal',
+        'weight': 'bold',
+        'size': 18}
+
+matplotlib.rc('font', **font)
 
 length = 20
 x = np.linspace(0, length, length+1)
@@ -14,5 +21,6 @@ plt.rc("font", family="Serif")
 plt.plot(x, y, marker='o', markersize=20)
 plt.xlabel("Step Number")
 plt.ylabel("Position")
+plt.tight_layout()
 plt.savefig("./Paper/images/cartoon.pdf")
 plt.show()
